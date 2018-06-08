6e2e506d9466bddf9e76950d1fd1fc49dd5f017f
cthach010 <cthach010@gmail.com>
2018-06-08 18:54:20 -0400

    users profile informantion

--- assets/javasript/firebase.js
+++
@@ -1,9 +0,0 @@
-var config = {
-    apiKey: "AIzaSyAobFG_1ucgVbs8O_pncspjeC7IImwfwPs",
-    authDomain: "what-to-do-829ed.firebaseapp.com",
-    databaseURL: "https://what-to-do-829ed.firebaseio.com",
-    projectId: "what-to-do-829ed",
-    storageBucket: "",
-    messagingSenderId: "138224366699"
-  };
-  firebase.initializeApp(config);
